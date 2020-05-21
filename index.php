500cfe1b6f5c325c139335f8a724b3002f1bce27
Your Name <you@example.com>
2020-05-21 09:08:52 +0700

    update

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include_once ("Stack.php");
+$stack = new Stack(10);
+$stack->push(1);
+$stack->push(2);
+$stack->push(3);
+$stack->push(4);
+$stack->push(5);
+$stack->pop();
+$stack->pop();
+$stack->pop();
+$stack->push(6);
+$stack->push(7);
+$stack->isEmpty();
+$stack->pop();
+$stack->pop();
+$stack->pop();
+$stack->pop();
+$stack->isEmpty();
